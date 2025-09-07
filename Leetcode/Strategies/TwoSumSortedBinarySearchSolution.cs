10bbd8af01377d3f77aee5f4abb76aa17fc6dbe1
Abhimanyu Madiraju <abhimanyu.builds@gmail.com>
2025-09-06 21:16:24 -0400

    Add strategies for TwoSumII (two-sum variant for sorted arrays)

---
+++ Leetcode/Strategies/TwoSumSortedBinarySearchSolution.cs
@@ -0,0 +1,54 @@
+﻿using Leetcode.Interfaces;
+using Leetcode.Models;
+
+namespace Leetcode.Strategies
+{
+    public class TwoSumSortedBinarySearchSolution : IProblemSolution<TwoSumSortedInput, int[]>
+    {
+        public int[] Solve(TwoSumSortedInput input)
+        {
+
+            //Submission: https://leetcode.com/submissions/detail/1762022829/
+
+            //Analysis: 
+            //array is sorted - in non-decreasing order.
+            //return indices+1, not direct indices.
+            //no duplicates
+            //need not account for scenario with no solution.
+
+            //Approach: 
+            //track indices. if sum is more than target, window must reduce. else increase
+            //for -ve numbers, it should be opposite
+            //use binary search for complement
+            //for loop or while for readability
+
+            //trivial example test case: [10, 20, 30, 40, 50, 60];
+            //example target 100. result for 40,60 []
+            var (numbers, target) = (input.Numbers, input.Target);
+            for (int i = 0; i < numbers.Length-1; i++) //n-1 as last element cannot have a complement
+            {
+                if (i > 0 && numbers[i] == numbers[i - 1]) continue;    //skip duplicates
+                int complement = target - numbers[i];
+                int left = i + 1, right = numbers.Length - 1;
+                while (left <= right)
+                {
+                    int mid = left + (right - left) / 2; //mathematically equivalent but computationally safer, avoids overflow for large numbers
+                    if (numbers[mid] == complement)
+                    {
+                        return [i + 1, mid + 1];
+                    }
+                    else if (numbers[mid] < complement)
+                    {
+                        left = mid + 1;
+                    }
+                    else
+                    {
+                        right = mid - 1;
+                    }
+                }
+            }
+            // Unreachable line due to problem constraints (exactly one solution guaranteed).
+            return [-1, -1];
+        }
+    }
+}
---
+++ Leetcode/Strategies/TwoSumSortedSolution.cs
@@ -0,0 +1,44 @@
+﻿using Leetcode.Interfaces;
+using Leetcode.Models;
+
+namespace Leetcode.Strategies
+{
+    public class TwoSumSortedSolution : IProblemSolution<TwoSumSortedInput, int[]>
+    {
+        public int[] Solve(TwoSumSortedInput input)
+        {
+
+            //Submission: https://leetcode.com/submissions/detail/1762007111/
+
+            //Analysis: 
+            //array is sorted - in non-decreasing order.
+            //return indices+1, not direct indices.
+            //no duplicates
+            //need not account for scenario with no solution.
+
+            //Approach: 
+            //track indices. if sum is more than target, window must reduce. else increase
+            //for loop or while for readability
+            //only extremes as starting indices will work.
+            //taking random starting indices might not as valid pairs might fall outside the window
+
+            //trivial example test case: [10, 20, 30, 40, 50, 60];
+            //example target 100. result for 40,60 []
+            var (numbers, target) = (input.Numbers, input.Target);
+            int left = 0, right = numbers.Length - 1;
+            while (numbers[left] + numbers[right] != target && left < right)
+            {
+                if (numbers[left] + numbers[right] < target)
+                {
+                    left++;
+                }
+                else
+                {
+                    right--;
+                }
+            }
+            return [left + 1, right + 1];
+
+        }
+    }
+}
